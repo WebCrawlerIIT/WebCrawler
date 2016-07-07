f0807e5226c330d9b00b42a97c0778a7eabcb437
Mateus Lopes Teixeira <lopesteixeiramateus@gmail.com>
2016-07-07 11:36:59 -0500

    Paralellization of the ImageCrawler.

--- source/src/webCrawler/ImageCrawler.java
+++ source/src/webCrawler/ImageCrawler.java
@@ -8,36 +8,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.InputStream;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.net.UnknownHostException;
 
 /**
  * Class used from crawling images from Google Search
  * @author Mateus Lopes Teixeira, Michael Sampietro
  *
  */
-public class ImageCrawler {
+public class ImageCrawler implements Runnable {
 	
 	private int num 			= 10;
 	private int startIndex 		= 1;
 	private String url 			= "https://www.googleapis.com/customsearch/v1?q=";
-	private String key 			= "AIzaSyBn2GPcApdNEwdClTXepws2RKdvsPPLdFs";
-	private String cx 			= "014112626754147301237:delvakx2lme";
+	private String key 			= "AIzaSyBn2GPcApdNEwdClTXepws2RKdvsPPLdFs";	// MINHA CONTA
+	private String cx 			= "014112626754147301237:delvakx2lme";			// MINHA CONTA
+//	private String key 			= "AIzaSyAWskVyL0vUTiMHbzuKdLR6FSUBDBZB7Q4";	// CONTA IIT
+//	private String cx 			= "016812031941283514304:1yphssixquu";			// CONTA IIT
+
 	private String searchType	= "image";
 	private String fileType 	= "jpg";
 	
+	private int errors 			= 0;	// Number of errors that occured while downloading images
+	private int results 		= 20;	// Number of results that we want to save
+	private int numberOfQueries = (int) Math.ceil((double) results / (double) num); // Necessary number of queries to download the amount of images desired
+	
+	public int id;
+	public String searchQuery;
+	//private final String searchQuery;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setSearchQuery(String sq) {
+		this.searchQuery = sq;
+	}
 	public ImageCrawler() {
+
+	}
+	public ImageCrawler (int id) 
+	{
+		this.setId(id);
+		searchQuery = getSearchQuery();
+		run();
+	}
 		
+	public void run() {
+		boolean resultsDirectory = new File("results").mkdir();
+
+		System.out.println(" Query from inside run: " + searchQuery);
+		
+		//String query = getSearchQuery();
+		getJSONfromQuery(searchQuery);
 	}
 	
 	/**
@@ -45,8 +81,9 @@ public class ImageCrawler {
 	 * @return a String containing the query
 	 */
 	public String getSearchQuery() {
+				
 		String query = "";
-		System.out.println("Query: ");
+		System.out.print("Query: ");
 		
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -60,31 +97,33 @@ public class ImageCrawler {
 	}
 	
 	
-	// TODO: Check that the result of the query was 200, and not something else
 	/**
 	 * Using the query submitted by the user, searches Google Images
 	 * and returns a JSON containing the results. This JSON can be further explored
 	 * by changing the "index", being 1 the 1st result in the 1st page, 11 the 1st result
-	 * in the 2nd page, and so further.
+	 * in the 2nd page, and so on.
 	 * @param query - a String containing the user query
 	 * @return a JSONObject with the result returned by Google
 	 */
 	public JSONObject getJSONfromQuery(String query) {
 		
+		String searchTerms = query.replaceAll(" ", "_");
 		JSONObject resultJSON = null;
+		
 		try {
 			resultJSON = new JSONObject();
 			JSONParser parser = new JSONParser();
 			
-			for(int i = 0; i < 5; i++) {
-				String urlString = url + query + "&key=" + key + "&cx=" + cx 
-						+ "&searchType=" + searchType  + "&start=" + startIndex + "&fileType=" + fileType;
+			for(int i = 0; i < numberOfQueries; i++) {
 				
-				System.out.println(urlString);
+				String urlString = generateURL(query);
 				URL url = new URL(urlString);
-				URLConnection connection = url.openConnection();
+				//URLConnection connection = url.openConnection();
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 				
-				connection.setRequestProperty("User-Agent", "WebCrawler IIT");
+				//connection.setRequestProperty("User-Agent", "WebCrawler IIT");
+				connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			    connection.setRequestProperty("Accept", "application/json");
 				connection.connect();
 				
 				InputStream is = connection.getInputStream();
@@ -93,18 +132,12 @@ public class ImageCrawler {
 				
 				resultJSON = (JSONObject) parser.parse(jsonString);
 							
-//				ArrayList link1 = ((ArrayList) resultJSON.get("items"));
-//				int size = link1.size();
-//				System.out.println("size: " + size);
+				getImageAttributesFromJSON(resultJSON, startIndex, query);
 				
-				ImageAttributes img = new ImageAttributes();
-				getImageAttributes(img, resultJSON, startIndex);
-				
-				startIndex += 10;
-				
+				startIndex += 10;	// Sets the startIndex variable to the first result in the next page
 			}
+			
 			return resultJSON;
-
 			
 		} catch (MalformedURLException mue) {
 			mue.printStackTrace();
@@ -117,22 +150,17 @@ public class ImageCrawler {
 		return resultJSON;
 	}
 	
+
 	/**
-	 * From the JSON object returned by Google Images, this method extracts all the information
-	 * from it, like the link to the JPEG and the caption. 
-	 * @param image
-	 * @param json
+	 * Extracts all the information about the images from the JSON returned by Google Images.
+	 * @param image: an ImageAttributes instance
+	 * @param json: a JSONObject instance
+	 * @param startIndex: the startIndex used while querying Google Images
 	 */
-	public void getImageAttributes(ImageAttributes image, JSONObject json, int startIndex) {
-		
-		// Gets the search terms used for querying Google, and makes them the name of the folder
-		// that will contain all the results returned by Google
-		JSONObject queries = (JSONObject) json.get("queries");
-		ArrayList<JSONObject> request = (ArrayList<JSONObject>) queries.get("request");
-		String searchTerms = (String) request.get(0).get("searchTerms");
-		searchTerms.replaceAll(" ", "_").toLowerCase(); 
-		image.setSearchTerms(searchTerms);
-		System.out.println("SEARCH TERMS: " + image.getSearchTerms());
+	public void getImageAttributesFromJSON(JSONObject json, int startIndex, String query) {
+			
+		ImageAttributes image = new ImageAttributes();
+		image.setSearchTerms(query.replaceAll(" ", "+"));
 		
 		// Gets an array of results, that will be iterated on and the informations about the 
 		// images will be extracted and then downloaded
@@ -145,49 +173,69 @@ public class ImageCrawler {
 			JSONObject item = iterator.next();
 			String link = (String) item.get("link");
 			String snippet = (String) item.get("snippet");
-			//String contextLink = (String) item.get("contextLink");
 			
 			image.setLink(link);
 			image.setSnippet(snippet);
-			image.setSearchTerms(searchTerms);
-			//image.setContextLink(contextLink);
 			
-			
-			System.out.println("Search terms: " + image.getSearchTerms());
-			boolean queryResultsDir = new File("results/" + image.getSearchTerms()).mkdir();
+			boolean queryResultsDir = new File("../results/" + image.getSearchTerms()).mkdir();
 			downloadImage(image, index);
+			
 			index++;
-			//System.out.println("Link: " + image.getLink());
-			//System.out.println("\tSnippet: " + image.getSnippet());
-			//System.out.println("\tContextLink: " + image.getContextLink());
 		}
 	}
 	
 	private void downloadImage(ImageAttributes image, int index) {
 		
-		System.out.println("ID: " + index + " - Downloading " + image.getLink() + "...");
-		System.out.println("\tSnippet: " + image.getSnippet());
-		URL url;
-		
-		String filename = index + ".jpg";
-		String path = "results/" + image.getSearchTerms() + "/";
-		
 		try {	
-			url = new URL(image.getLink());
-			InputStream in = new BufferedInputStream(url.openStream());
-			OutputStream out = new BufferedOutputStream(new FileOutputStream(path + filename));
+			int newIndex;
+			int stopCondition = results - errors; // Stops the program when the necessary amount of images has been downloaded
 			
-			int readByte;
-			while ((readByte = in.read()) != -1) {
-				out.write(readByte);
+			if((index - errors) == 0) 
+				newIndex = startIndex + 1;
+			else 
+				newIndex = index - errors;
+			
+			if(newIndex <= stopCondition) {
+
+				URL url = new URL(image.getLink());
+				HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+				connection.connect();
+
+				int responseCode = connection.getResponseCode();
+				System.out.println("Code: " + responseCode);
+				if(responseCode != 200)
+					throw new IOException();
+				
+				System.out.println("(" + id + ")" + " IndexReal: " + index + " |newIndex: " + newIndex + " |Results: " + results);
+				String filename = newIndex + ".jpg";
+				String path = "../results/" + image.getSearchTerms() + "/";
+
+				InputStream in	 = new BufferedInputStream(connection.getInputStream());
+				OutputStream out = new BufferedOutputStream(new FileOutputStream(path + filename));
+
+				int readByte;
+				while ((readByte = in.read()) != -1) {
+					out.write(readByte);
+				}
+
+				in.close();
+				out.close();
 			}
 			
-			in.close();
-			out.close();
-			
+		} catch (UnknownHostException uhe) {
+			System.out.println("===UNKNOWN HOST EXCEPTION===");
+			errors++;
+			results += 1;
+			numberOfQueries = (int) Math.ceil((double) results / (double) num);
+			//uhe.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			System.out.println("===IOEXCEPTION===");
+			errors++;
+			results += 1;
+			numberOfQueries = (int) Math.ceil((double) results / (double) num);
+			System.out.println(image.getLink());
+			//e.printStackTrace();
+		} 
 	}
 	
 	/**
@@ -205,4 +253,31 @@ public class ImageCrawler {
 	    }
 	    return stringBuilder.toString();
 	  }
+	
+	private String generateURL(String query) {
+		
+		String queryEncoded;
+		
+		try {
+			queryEncoded = URLEncoder.encode(query, "UTF-8").replaceAll("\\%28", "(") 
+					.replaceAll("\\%29", ")") 
+					.replaceAll("\\+", "%20") 
+					.replaceAll("\\%27", "'") 
+					.replaceAll("\\%21", "!") 
+					.replaceAll("\\%7E", "~");
+			
+			String urlString = url + queryEncoded + "&key=" + key + "&cx=" + cx 
+					+ "&searchType=" + searchType  + "&start=" 
+					+ startIndex + "&fileType=" + fileType;
+			
+			System.out.println(urlString);
+			return urlString;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
 }
+
--- source/src/webCrawler/WebCrawler.java
+++ source/src/webCrawler/WebCrawler.java
@@ -21,37 +21,59 @@ public class WebCrawler {
 	//protected LinkedList<Webpage> crawlFrontier = new LinkedList<Webpage>();
 	
 	public static void main(String[] args) {
-		
+
+
 		LinkedList<Webpage> seeds = new LinkedList<Webpage>();
 		LinkedList<Webpage> crawlFrontier = new LinkedList<Webpage>();
 		
 		HTMLParser parser = new HTMLParser();
 		Webpage webpage = new Webpage();
-		ImageCrawler imageCrawler = new ImageCrawler();
 		
-		String query = imageCrawler.getSearchQuery();
-		imageCrawler.getJSONfromQuery(query);
-	
-		boolean resultsDirectory = new File("results").mkdir();
-				
-		try {
-			importSeedsFromFile(seeds);
+		// The number of available cores for processing will determine how many queries will run 
+		// at the same time
+		int availableCores = Runtime.getRuntime().availableProcessors();
+		System.out.println("Number of available cores: " + availableCores);
+		
+		// Definition of the imageCrawlers and Threads that will parallelize the search.
+		// Once again, the amount of crawlers and threads will be defined by the available cores
+		ImageCrawler[] imageCrawlers = new ImageCrawler[availableCores];
+		Thread[] threads = new Thread[availableCores];
+		
+		// Instatiation of each image crawler and each thread, as well as linking them both
+		for(int i = 0; i < availableCores; i++) {			
+			imageCrawlers[i] = new ImageCrawler();
+			imageCrawlers[i].setId(i);
+			String query = imageCrawlers[i].getSearchQuery();
+			imageCrawlers[i].setSearchQuery(query);
 			
-			while(!seeds.isEmpty()) {
-				Iterator<Webpage> iterator = seeds.iterator();
-				
-				while(iterator.hasNext()) {
-					webpage = seeds.removeFirst();
-					parser.downloadAndParsePage(webpage);
-					
-					String linksFile = webpage.getLinksFile();
-					//importLinksFromFile(seeds, webpage);
-				}
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
+			threads[i] = new Thread(imageCrawlers[i]);
 		}
+		
+		// Starts all the threads so they run in parallel
+		for(int i = 0; i < availableCores; i++) 
+			threads[i].start();
+		
+			
+		//boolean resultsDirectory = new File("results").mkdir();
+				
+//		try {
+//			importSeedsFromFile(seeds);
+//			
+//			while(!seeds.isEmpty()) {
+//				Iterator<Webpage> iterator = seeds.iterator();
+//				
+//				while(iterator.hasNext()) {
+//					webpage = seeds.removeFirst();
+//					parser.downloadAndParsePage(webpage);
+//					
+//					String linksFile = webpage.getLinksFile();
+//					//importLinksFromFile(seeds, webpage);
+//				}
+//			}
+//				
+//			} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 	private static void importSeedsFromFile(LinkedList<Webpage> seeds) throws FileNotFoundException, IOException {
